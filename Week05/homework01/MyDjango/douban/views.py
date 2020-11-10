ad19125351fe0673ebd07344acc14a8c5596e414
Yx <310914495@qq.com>
2020-11-10 23:07:35 +0800

    fix_bug

--- Week05/homework01/MyDjango/douban/views.py
+++ Week05/homework01/MyDjango/douban/views.py
@@ -7,7 +7,7 @@ def index(request):
     search = request.GET.get('q')
     if not search:
         search = '金刚川'
-    comments = Comments.objects.filter(movie_name=search).filter(stars__gte=3).order_by('stars').reverse()
+    comments = Comments.objects.filter(movie_name=search).filter(stars__gt=3).order_by('stars').reverse()
     movie_name = Comments.objects.filter(movie_name=search).values('movie_name').first()
     if not movie_name:
         movie_name = ''
